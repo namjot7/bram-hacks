e80078f1b800489e3a6c7abe94fec9ff2e3e68a7
Namjot Singh <namjot777@gmail.com>
2024-11-08 21:26:25 -0500

    creating components folder

---
+++ app/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h2 className=" text-2xl font-bold py-4 bg-gray-800 ">Hi World</h2>
+            <div className="">
+
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
--- app/page.js
+++ app/page.js
@@ -12,12 +12,7 @@ export default function Home() {
   return (
     <>
       <div className="bg-gray-950 min-h-screen text-white">
-        <nav>
-          <h2 className=" text-2xl font-bold py-4 bg-gray-800 ">Hi World</h2>
-          <div className="">
-
-          </div>
-        </nav>
+        
         <main>
           
         </main>
