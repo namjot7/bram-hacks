163d5e633277c731e25291d7522683b298097733
Namjot Singh <namjot777@gmail.com>
2024-11-08 21:15:24 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -13,9 +13,9 @@ export default function Home() {
     <>
       <div className="bg-gray-950 min-h-screen text-white">
         <nav>
-          <h2 className=" text-2xl font-bold py-4 bg-gray-800 ">Uber</h2>
+          <h2 className=" text-2xl font-bold py-4 bg-gray-800 ">Hi World</h2>
           <div className="">
-            
+
           </div>
         </nav>
         <main>
